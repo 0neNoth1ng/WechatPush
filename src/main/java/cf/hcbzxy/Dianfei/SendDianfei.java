c302c03a49a7e89b5ca449cce264b63161321c8f
OneNoth1ng <3602869979@qq.com>
2022-08-31 22:12:51 +0800

    寝室电费推送代码完工

--- src/main/java/cf/hcbzxy/Dianfei/SendDianfei.java
+++ src/main/java/cf/hcbzxy/Dianfei/SendDianfei.java
@@ -1,10 +1,10 @@
-package cf.hcbzxy;
+package cf.hcbzxy.Dianfei;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -13,17 +13,11 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Date;
 
-@SpringBootTest
-class WechatPushApplicationTests {
+@Component
+public class SendDianfei {
 
-    @Test
-    void contextLoads() {
-    }
-
-    @Autowired
-    JavaMailSender javaMailSender;
-    @Test
-    public void sendSimpleMail() {
+    public static String getDianfei() {
+        //java环境中文传值时，需特别注意字符编码问题
         String httpUrl = "https://wap.itzo.cn/api/519.php";
 
         BufferedReader reader = null;
@@ -56,15 +50,28 @@ class WechatPushApplicationTests {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return diangfei;
+    }
 
+    @Autowired
+    JavaMailSender javaMailSender;
+
+
+    public void sendSimpleMail() {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setSubject("电费来咯！");
+        message.setSubject("电费来咯");
         message.setFrom("1628880266@qq.com");
         message.setTo("3602869979@qq.com");
         message.setSentDate(new Date());
-        message.setText(diangfei);
+        message.setText(getDianfei());
         javaMailSender.send(message);
     }
 
+
+    @Scheduled(cron = "0 0 8 * * ?") // 每天早上八点准时发
+    //@Scheduled(cron = "0 10 22 * * ?")  //测试每十秒发一次
+    public void Go(){
+        sendSimpleMail();
+    }
 
 }
